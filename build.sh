729791fe7d7ea00e2a64e74c79ec597558f4a107
ZaraKnYu <razenxizu@gmail.com>
2026-05-15 22:40:05 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Repo Init
-repo init -u https://github.com/LineageOS/android.git -b lineage-23.2 --git-lfs
+repo init -u https://github.com/Evolution-X/manifest -b bq2 --git-lfs
 /opt/crave/resync.sh
 
 rm -rf device/motorola/mumba
@@ -9,8 +9,8 @@ rm -rf vendor/motorola/mumba
 rm -rf device/motorola/mumba-kernel
 rm -rf hardware/motorola
 
-git clone https://github.com/motorola-sm6435/android_device_motorola_mumba device/motorola/mumba
-git clone https://github.com/motorola-sm6435/android_vendor_motorola_mumba vendor/motorola/mumba
+git clone https://github.com/motorola-sm6435/android_device_motorola_mumba -b test device/motorola/mumba
+git clone https://github.com/motorola-sm6435/android_vendor_motorola_mumba -b test vendor/motorola/mumba
 git clone https://github.com/motorola-sm6435/android_device_motorola_mumba-kernel device/motorola/mumba-kernel
 git clone https://github.com/LineageOS/android_hardware_motorola -b lineage-23.2 hardware/motorola
 
