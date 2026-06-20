300d477a803462cfd75acbde0595aaa665f3c869
ZaraKnYu <razenxizu@gmail.com>
2026-06-21 02:14:47 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -10,13 +10,13 @@ rm -rf vendor/motorola/mumba
 rm -rf device/motorola/mumba-kernel
 rm -rf hardware/motorola
 
-git clone https://github.com/motorola-sm6435/android_device_motorola_mumba -b test device/motorola/mumba
+git clone https://github.com/motorola-sm6435/android_device_motorola_mumba device/motorola/mumba
 git clone https://github.com/motorola-sm6435/proprietary_vendor_motorola_mumba vendor/motorola/mumba
-git clone https://github.com/motorola-sm6435/android_device_motorola_mumba-kernel -b test device/motorola/mumba-kernel
+git clone https://github.com/motorola-sm6435/android_device_motorola_mumba-kernel device/motorola/mumba-kernel
 git clone https://github.com/LineageOS/android_hardware_motorola -b lineage-23.2 hardware/motorola
 
 . build/envsetup.sh
-lunch lineage_mumba-bp4a-eng
+lunch lineage_mumba-bp4a-userdebug
 mka bacon
 
 git clone https://github.com/motorola-sm6435/android_device_motorola_mumba device/motorola/mumba
