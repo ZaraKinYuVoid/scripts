9170b6e0895540efc81a59cf6d1a42c36229e400
ZaraKnYu <razenxizu@gmail.com>
2026-06-22 02:15:56 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -11,17 +11,11 @@ rm -rf device/motorola/mumba-kernel
 rm -rf hardware/motorola
 
 git clone https://github.com/motorola-sm6435/android_device_motorola_mumba device/motorola/mumba
-git clone https://github.com/motorola-sm6435/proprietary_vendor_motorola_mumba vendor/motorola/mumba
+git clone https://github.com/motorola-sm6435/android_vendor_motorola_mumba vendor/motorola/mumba
+git clone https://github.com/motorola-sm6435/packages_apps_ViPER4AndroidFX packages/apps/ViPER4AndroidFX
 git clone https://github.com/motorola-sm6435/android_device_motorola_mumba-kernel device/motorola/mumba-kernel
 git clone https://github.com/LineageOS/android_hardware_motorola -b lineage-23.2 hardware/motorola
 
 . build/envsetup.sh
 lunch lineage_mumba-bp4a-userdebug
-mka bacon
-
-git clone https://github.com/motorola-sm6435/android_device_motorola_mumba device/motorola/mumba
-git clone https://github.com/motorola-sm6435/proprietary_vendor_motorola_mumba vendor/motorola/mumba
-git clone https://github.com/motorola-sm6435/android_kernel_motorola_sm6435 kernel/motorola/sm6435
-git clone https://github.com/motorola-sm6435/android_kernel_motorola_sm6435-modules kernel/motorola/sm6435-modules
-git clone https://github.com/motorola-sm6435/android_kernel_motorola_sm6435-devicetrees kernel/motorola/sm6435-devicetrees
-git clone https://github.com/LineageOS/android_hardware_motorola -b lineage-23.2 hardware/motorola
+m evolution
