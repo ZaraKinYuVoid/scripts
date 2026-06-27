4677b953c41e6525b08a7f625ba520d4f2e332d9
ZaraKinYuVoid <razenxizu@gmail.com>
2026-06-27 15:05:41 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-mkdir -p evolutionX
-cd evolutionX
-repo init -u https://github.com/Evolution-X/manifest -b bq2 --git-lfs
+mkdir -p LineageOS
+cd LineageOS
+repo init -u https://github.com/LineageOS/android.git -b lineage-23.2--git-lfs
 repo sync -c -j$(nproc --all) --force-sync --no-clone-bundle --no-tags
 
 rm -rf device/motorola/mumba
@@ -10,12 +10,11 @@ rm -rf vendor/motorola/mumba
 rm -rf device/motorola/mumba-kernel
 rm -rf hardware/motorola
 
-git clone https://github.com/motorola-sm6435/android_device_motorola_mumba device/motorola/mumba
-git clone https://github.com/motorola-sm6435/android_vendor_motorola_mumba vendor/motorola/mumba
-git clone https://github.com/motorola-sm6435/packages_apps_ViPER4AndroidFX packages/apps/ViPER4AndroidFX
+git clone https://github.com/ZaraKinYuVoid/device_motorola_mumba device/motorola/mumba
+git clone https://github.com/ZaraKinYuVoid/vendor_motorola_mumba vendor/motorola/mumba
 git clone https://github.com/motorola-sm6435/android_device_motorola_mumba-kernel device/motorola/mumba-kernel
 git clone https://github.com/LineageOS/android_hardware_motorola -b lineage-23.2 hardware/motorola
 
 . build/envsetup.sh
 lunch lineage_mumba-bp4a-userdebug
-m evolution
+mka bacon -j$(nproc --all)
